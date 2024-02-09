87ffc107d3def4c2068d20f2675cb045755c4f34
stephen-afk <159510340+stephen-afk@users.noreply.github.com>
2024-02-09 15:49:55 +0800

    Update script.js

--- script.js
+++ script.js
@@ -27,7 +27,7 @@ noButton.addEventListener("click", function () {
 });
 
 function handleYesClick() {
-  titleElement.innerHTML = "Yayyy!! :3";
+  titleElement.innerHTML = "Yehey kiss moko😗";
   buttonsContainer.classList.add("hidden");
   changeImage("yes");
 }
